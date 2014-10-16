fa606822fe4a56fd07095ac91b8ea178f52ac08d
songyeep <songyeep@gmail.com>
2014-10-16 17:20:48 +0800

    added database and seeded info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    @delivery_time = Time.now + 2700
+
+
+
+    # params[:admin]
+
+    # render 'index'
+    # render 'under_construction'
+
+
   end
 end
