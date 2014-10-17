b35eeab67945b0662c82312972584437789cdcf8
songyeep <songyeep@gmail.com>
2014-10-17 15:30:21 +0800

    sampling menu for landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     @delivery_time = Time.now + 2700
+    @rec_sample = FoodItem.all.pluck("name").sample
 
 
-
-    # params[:admin]
-
-    # render 'index'
-    # render 'under_construction'
-
+    @item = FoodItem.all.sample
+    @western = FoodItem.where(cuisine: "Western").pluck("photo_path").sample
+    @asian = FoodItem.where(cuisine: "Asian").pluck("photo_path").sample
+    @adventurous = FoodItem.where(cuisine: "Adventurous").pluck("photo_path").sample
 
   end
 end
